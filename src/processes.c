29e9484f7d772579aec0220a9342e12a3c5dff88
Luiz Schonarth <luizcschonarth@gmail.com>
2022-09-13 15:41:23 -0300

    Adiciona funcionalidade para imprimir colorido

--- src/processes.c
+++ src/processes.c
@@ -27,21 +27,24 @@ p_list_t * parse_processes(char *filename) {
         fclose(p_file);
     } else {
         free(list);
-        fprintf(stderr, "[ERRO] Nao foi possivel abrir arquivo %s.\n", filename);
+        fprintf(stderr, COLOR_RED"[ERRO]"COLOR_RST" Nao foi possivel abrir o arquivo %s.\n", filename);
         exit(EXIT_FAILURE);
     }
     return list;
 }
 
+/* Imprime informação de debug para cada processo armazenado na lista. */
 void dump_processes(p_list_t * list) {
     for (size_t i = 0; i < list->size; i++) {
         process_t p = *list->processes[i];
-        printf("[DEBUG] ID do processo: %d\n", p.id);
-        printf("        Prioridade:     %d\n", p.priority);
-        printf("        Tempo de CPU:   %d\n\n", p.cpu_time);
+        printf(COLOR_YLW"[DEBUG]\n"COLOR_RST);
+        printf("    ID do processo: %d\n", p.id);
+        printf("    Prioridade:     %d\n", p.priority);
+        printf("    Tempo de CPU:   %d\n\n", p.cpu_time);
     }
 }
 
+/* Inicializa uma lista de processos com tamanho 0. */
 p_list_t * p_list_init(void) {
     p_list_t *list = NULL;
     list = (p_list_t *) alloc_or_panic(sizeof(p_list_t));
@@ -50,12 +53,14 @@ p_list_t * p_list_init(void) {
     return list;
 }
 
+/* Adiciona um processo na lista de processos. */
 void p_list_append(p_list_t *list, process_t *process) {
     list->size++;
     list->processes = (process_t**) realloc_or_panic(list->processes, list->size * sizeof(process_t*));
     list->processes[list->size - 1] = process;
 }
 
+/* Libera memória ocupada na heap pela lista de processos. */
 void p_list_destroy(p_list_t *list) {
     // Libera memória de cada processo da lista.
     for (size_t i = 0; i < list->size; i++) {
--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,10 @@
 
 /* Header para funções utilitárias que podem ser usadas no projeto. */
 
+#define COLOR_RED "\033[0;31m"
+#define COLOR_YLW "\033[0;33m"
+#define COLOR_RST "\033[0m"
+
 #include <stdlib.h>
 
 void * alloc_or_panic(size_t size);
